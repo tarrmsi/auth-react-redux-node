fba6000d7bfc3748bca7163f4398873a1c032eeb
tarrmsi <florinpotopea@yahoo.com>
2019-12-23 23:13:14 +0200

    added extra api security

--- server/config/express.js
+++ server/config/express.js
@@ -3,6 +3,9 @@ const morgan = require("morgan");
 const cors = require("cors");
 const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
 
 const { logs } = require("./vars");
 const passport = require("./passport");
@@ -13,9 +16,12 @@ const app = express();
 // Middleware
 app.use(morgan(logs));
 app.use(express.json());
+app.use(mongoSanitize());
 app.use(cookieParser());
 app.use(cors());
 app.use(helmet());
+app.use(xss());
+app.use(hpp());
 
 // Routes
 app.use("/api/auth", authRoutes);
